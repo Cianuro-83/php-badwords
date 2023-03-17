75061419d23f7e2aa0e30b122c3982f25405b2a2
Cianuro-83 <buzzeosimone@yahoo.com>
2023-03-17 17:57:45 +0100

    FIX: scaffold HTML5

--- censura.php
+++ censura.php
@@ -1,29 +1,35 @@
-<style>
-body {
-    background-color: black;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    padding: 25%;
-}
-</style>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-$testo = $_GET["testo"];
-$censura = $_GET["censura"];
-$testo_censurato = str_replace($censura, "***", $testo);
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords 2</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
 
-?>
+<body>
+    <div class="php">
 
-<h2>Questo è il testo originale: </h2>
-<p><?php echo $testo; ?></p>
-<h2>ed è composta da <?php echo strlen($testo); ?> lettere</h2>
+        <?php
+    $testo = $_GET["testo"];
+    $censura = $_GET["censura"];
+    $testo_censurato = str_replace($censura, "***", $testo);
+    
+    ?>
+
+        <h2>Questo è il testo originale: </h2>
+        <p><?php echo $testo; ?></p>
+        <h2>ed è composta da <span><?php echo strlen($testo); ?></span> lettere</h2>
 
 
-<h2>Questa invece è la parola da censurare: </h2>
-<p><?php echo $censura; ?></p>
+        <h2>Questa invece è la parola da censurare: </h2>
+        <p><?php echo $censura; ?></p>
 
-<h2>e questo è il testo censurato </h2>
-<p><?php echo $testo_censurato; ?></p>
+        <h2>e questo è il testo censurato </h2>
+        <p><?php echo $testo_censurato; ?></p>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,24 @@
-<style>
-body {
-    background-color: black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-</style>
+<!DOCTYPE html>
+<html lang="en">
 
-<form action="./censura.php" method="GET">
-    <textarea id="testo" name="testo" rows="5" cols="33" placeholder="Inserisci qui il tuo testo">
-</textarea>
-    <input type="text" name="censura" placeholder="Parola da censurare">
-    <button type="submit">INVIA</button>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+    <link rel="stylesheet" href="./style.css">
 
-</form>
+</head>
+
+<body>
+    <form action="./censura.php" method="GET">
+        <textarea id="testo" name="testo" rows="5" cols="33" placeholder="Inserisci qui il tuo testo">
+    </textarea>
+        <input type="text" name="censura" placeholder="Parola da censurare">
+        <button type="submit">INVIA</button>
+
+    </form>
+
+</body>
+
+</html>
